3ba58c6e2a43ea657cf84b64b77fe20d0c3b0e8b
Victor <victorgestaltung@gmail.com>
2025-04-17 19:04:14 -0600

    feat(fix): db ssl

--- db/config.js
+++ db/config.js
@@ -1,18 +1,15 @@
 const { config } = require('./../config/config');
 
-const { user, password, host, port, name } = config.db;
-
-const USER = encodeURIComponent(user)
-const PASSWORD = encodeURIComponent(password)
-const URI = `postgres://${USER}:${PASSWORD}@${host}:${port}/${name}`
-
 module.exports = {
   development: {
-    url: URI,
+    url: config.dbUrl,
     dialect: 'postgres',
   },
   production: {
-    url: URI,
+    url: config.dbUrl,
     dialect: 'postgres',
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
 }
--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -8,16 +8,21 @@ const DB_PASSWORD = password
 const DB_HOST = host
 const DB_PORT = port
 const DB_NAME = name
-let URI = ``
+
+const options = {}
 
 if (config.isProd){
-  URI = config.dbUrl
+  options.connectionString = config.dbUrl
+  options.ssl = {
+    rejectUnauthorized: false,
+  }
 } else {
   const USER = encodeURIComponent(DB_USER)
   const PASSWORD = encodeURIComponent(DB_PASSWORD)
-  URI = `postgres://${USER}:${PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
+  const URI = `postgres://${USER}:${PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
+  options.connectionString = URI
 }
 
-const pool = new Pool({connectionString: URI});
+const pool = new Pool(options);
 
 module.exports = pool;
