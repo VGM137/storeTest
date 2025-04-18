8e16891ea3ce8dcb057d09828560f05273cea83e
Victor <victorgestaltung@gmail.com>
2025-04-18 01:54:41 -0600

    feat(fix): migrations init

--- db/migrations/20250418073614-initial-model.js
+++ db/migrations/20250418073614-initial-model.js
@@ -4,7 +4,7 @@ const { UserSchema, USER_TABLE } = require('../models/user.model');
 const { CustomerSchema, CUSTOMER_TABLE } = require('../models/customer.model');
 const { CategorySchema, CATEGORY_TABLE } = require('../models/category.model');
 const { ProductSchema, PRODUCT_TABLE } = require('../models/product.model');  
-const { OrderSchema, ORDER_TABLE } = require('../models/order.model');
+const { ORDER_TABLE } = require('../models/order.model');
 const { OrderProductSchema, ORDER_PRODUCT_TABLE } = require('../models/order-product.model');
 
 /** @type {import('sequelize-cli').Migration} */
@@ -14,7 +14,46 @@ module.exports = {
     await queryInterface.createTable(CUSTOMER_TABLE, CustomerSchema);
     await queryInterface.createTable(CATEGORY_TABLE, CategorySchema);
     await queryInterface.createTable(PRODUCT_TABLE, ProductSchema);
-    await queryInterface.createTable(ORDER_TABLE, OrderSchema);
+    await queryInterface.createTable(ORDER_TABLE, {
+      id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: DataTypes.INTEGER
+      },
+      userId: {
+        field: 'user_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: CUSTOMER_TABLE,
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      status: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        defaultValue: 'pending'
+      },
+      deliveryType: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        defaultValue: 'shipping'
+      },
+      deliveryAddress: {
+        field: 'delivery_address',
+        allowNull: false,
+        type: DataTypes.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW
+      }
+    });
     await queryInterface.createTable(ORDER_PRODUCT_TABLE, OrderProductSchema);
   },
 
