dbefa0afd6e9a00d5213e946c4592cc3c5c1e00e
Victor <victorgestaltung@gmail.com>
2025-04-18 00:32:30 -0600

    feat(fix): migrations

--- db/config.js
+++ db/config.js
@@ -3,11 +3,11 @@ const { config } = require('./../config/config');
 module.exports = {
   development: {
     url: config.dbUrl,
-    dialect: 'postgres',
+    dialectOptions: 'postgres',
   },
   production: {
     url: config.dbUrl,
-    dialect: 'postgres',
+    dialectOptions: 'postgres',
     ssl: {
       rejectUnauthorized: false,
     },
